632d3976a9dd6cf52ca2845fbf5bc799e7fc3efe
vbcca <vybccal@gmail.com>
2018-02-26 21:15:54 +0200

    make contract ownable

--- contracts/TaxiToken.sol
+++ contracts/TaxiToken.sol
@@ -1,8 +1,9 @@
 pragma solidity ^0.4.18;
 
 import 'zeppelin-solidity/contracts/token/ERC20/CappedToken.sol';
+import 'zeppelin-solidity/contracts/ownership/Ownable.sol';
 
-contract TaxiToken is CappedToken { 
+contract TaxiToken is CappedToken, Ownable {
 
   string public constant name = "TaxiToken";
   string public constant symbol = "TAXI";
